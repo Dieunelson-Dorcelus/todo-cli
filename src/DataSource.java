10af1972d9416b982a43324e9da03f3c87cac8a2
Salayna <salaynadoukoure@gmail.com>
2020-12-12 15:03:08 +0100

    Created the parser and the  DataSource Class

---
+++ src/DataSource.java
@@ -0,0 +1,10 @@
+import parser.JSONParser;
+
+public class DataSource {
+	public <T> T getInstance(String name, JSONParser type) {
+		return null;
+	}
+	public void save(String name, JSONParser type) {
+		
+	}
+}
