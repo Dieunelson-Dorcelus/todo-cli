f16439ab92e1f84cd01ca2d911f91a417d9d8bde
Dieunelson Dorcelus <43044653+dd701116@users.noreply.github.com>
2020-12-14 13:51:54 +0100

    Generate javadoc and save diagram

--- docs/javadocs/package-search-index.js
+++ docs/javadocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"cli"},{"l":"command"},{"l":"repository"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"cli"},{"l":"command"},{"l":"parser"},{"l":"repository"}]
