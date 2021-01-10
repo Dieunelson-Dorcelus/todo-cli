39311d4f667cade6d8f24b019c526beda161d814
Dieunelson Dorcelus <43044653+dd701116@users.noreply.github.com>
2021-01-10 03:28:42 +0100

    Add Datasource with datasource test

--- src/repository/DataSource.java
+++ src/repository/DataSource.java
@@ -2,11 +2,27 @@ package repository;
 
 import parser.JSONParser;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 public class DataSource <T> {
-	public T getInstance(String name, JSONParser type) {
-		return null;
+
+	public T getInstance(Path name, JSONParser<T> parser) throws IOException {
+		List<String> content = Files.readAllLines(name, StandardCharsets.UTF_8);
+		StringBuilder builder = new StringBuilder();
+		for (String s: content) {
+			builder.append(s);
+		}
+		return parser.getObject(builder.toString());
 	}
-	public void save(String name, JSONParser type) {
-		
+	public void save(T object, Path name, JSONParser<T> parser) throws IOException {
+		BufferedWriter writer = new BufferedWriter(new FileWriter(String.valueOf(name)));
+		writer.write(parser.parse(object));
+		writer.close();
 	}
 }
