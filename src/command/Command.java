e74ea57f22df8e617affc6506f22e3ce67d24d13
Dieunelson Dorcelus <43044653+dd701116@users.noreply.github.com>
2021-02-01 11:23:49 +0100

    feat(Command): manage error in the parrent class

--- src/command/Command.java
+++ src/command/Command.java
@@ -1,16 +1,26 @@
 package command;
 
 
+import cli.CLI;
+
 /**
  * Command abstract class
  */
 public abstract class Command {
-
     /**
      * Run command
      * @param args
      */
-    public abstract void run(String[] args);
+    public abstract void run(String[] args) throws DefaultCommandException;
+
+    public void execute(String[] args){
+        try {
+            run(args);
+        }catch (DefaultCommandException exception){
+            CLI.print(exception.getMessage());
+            CLI.LOCK = false;
+        }
+    }
 
     /**
      * Show help
